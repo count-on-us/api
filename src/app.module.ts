c0caa189c1a6f0fe0d3bd2565a5caad819e4f027
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-31 19:50:19 -0300

    configures ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -9,7 +10,8 @@ import { UsersModule } from './users/users.module';
   imports: [
     AuthModule,
     UsersModule,
-    MongooseModule.forRoot('')
+    MongooseModule.forRoot(''),
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
