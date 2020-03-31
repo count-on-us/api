609dd38b24bc6f2e953ea35e06dc20debe8e826d
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-31 20:20:59 -0300

    configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
-    MongooseModule.forRoot(''),
-    ConfigModule.forRoot(),
+    MongooseModule.forRoot(configuration().mongoURI),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration]
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
