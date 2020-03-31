6d3e9c1737a5d95b70b3ab0a71525b2bab90a56c
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-31 20:33:33 -0300

    refactor condigurations

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,12 +1,5 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   secret: process.env.SECRET,
-  database: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10) || 27017,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    name: process.env.DB_NAME
-  },
   mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin&w=1`
 });
