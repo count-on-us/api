893d09a6eb10603a5b7507b00cf24dd0bb61a4e4
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-31 19:49:01 -0300

    configures mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    MongooseModule.forRoot('')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
