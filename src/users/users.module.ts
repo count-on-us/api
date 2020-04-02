df709d69abec1a055bd8dcb0f048acb76e2dd36e
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-01 22:36:00 -0300

    creates user controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UserSchema } from './schemas/user.schema';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
