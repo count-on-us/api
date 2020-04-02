a85fc575de80745d0e65828ea9d809c1391d01cb
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-01 22:15:03 -0300

    add passportLocalMongoose plugin

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,10 +1,11 @@
 import * as mongoose from 'mongoose';
+import * as passportLocalMongoose from 'passport-local-mongoose';
 
 export const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
-  breed: String,
   registerNumber: String,
   phone: String,
   password: String,
 });
+UserSchema.plugin(passportLocalMongoose);
