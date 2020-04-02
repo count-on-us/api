cf1e0dda959b565cbc7b363a462945ef3ba89ef3
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-01 22:42:34 -0300

    fix indentation

--- src/users/interfaces/user.interface.ts
+++ src/users/interfaces/user.interface.ts
@@ -1,8 +1,8 @@
 import { Document, PassportLocalDocument } from 'mongoose';
 
 export interface IUser extends PassportLocalDocument {
-    readonly firstName: string;
-    readonly lastName: string;
-    readonly email: string;
-    readonly password: string;
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly email: string;
+  readonly password: string;
 }
