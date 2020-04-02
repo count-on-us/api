f9754d31bcfdb08b99a66ec3d884f4f9e570a0c2
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-02 19:44:46 -0300

    configures user module to use typeORM

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,36 +1,14 @@
 import * as passportLocalMongoose from 'passport-local-mongoose';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
-import { UserSchema } from './schemas/user.schema';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  imports: [MongooseModule.forFeatureAsync([
-    {
-      name: 'User',
-      useFactory: () => {
-        const schema = UserSchema;
-
-        schema.plugin(passportLocalMongoose);
-
-        return schema;
-      }
-    }
-  ])],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
-  exports: [UsersService, MongooseModule.forFeatureAsync([
-    {
-      name: 'User',
-      useFactory: () => {
-        const schema = UserSchema;
-
-        schema.plugin(passportLocalMongoose);
-
-        return schema;
-      },
-    },
-  ])],
+  exports: [UsersService],
   controllers: [UsersController],
 })
 export class UsersModule {}
