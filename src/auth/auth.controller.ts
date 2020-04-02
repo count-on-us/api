75590035ce147d1b9a1321d1757ec2fce446e4e4
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-01 23:34:12 -0300

    creates auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, HttpStatus, Response, Post, Body } from '@nestjs/common';
+import { LoginUserDto } from '../users/dtos/login-user.dto';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { UsersService } from '../users/users.service';
+import { IUser } from 'src/users/interfaces/user.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService,
+                private readonly usersService: UsersService) {}
+
+    @Post('register')
+    public async register(@Response() res, @Body() user: IUser){
+        const result = await this.authService.register(user);
+        if (!result.success){
+            return res.status(HttpStatus.BAD_REQUEST).json(result);
+        }
+        return res.status(HttpStatus.OK).json(result);
+    }
+
+    @Post('login')
+    @UseGuards(AuthGuard('local'))
+    public async login(@Response() res, @Body() login: LoginUserDto){
+        return await this.usersService.findOne({ username: login.email}).then(user => {
+            if (!user) {
+                res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                    message: 'User Not Found',
+                });
+            } else {
+                console.log('start getting the token');
+                const token = this.authService.createToken(user);
+                console.log(token);
+                return res.status(HttpStatus.OK).json(token);
+            }
+        });
+    }
+}
