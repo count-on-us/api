d76be44d4ef117df275b789942701bf4f4fc23c3
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-01 23:33:59 -0300

    moves jwt to passport folder

---
+++ src/auth/passport/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'ILovePokemon',
+    });
+  }
+
+  // tslint:disable-next-line:ban-types
+  async validate(payload: JwtPayload) {
+    const user = await this.authService.validateUser(payload);
+    if (!user) {
+      return new UnauthorizedException();
+    }
+    return user;
+  }
+}
