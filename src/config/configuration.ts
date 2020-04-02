906253c525dd337c6cd2e845245cc6abb237cfac
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-02 19:17:12 -0300

    update configuration for postgres

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,14 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   secret: process.env.SECRET,
-  mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin&w=1`
+  databasse: {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }
 });
