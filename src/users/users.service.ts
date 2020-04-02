9fb76e88b1e81e90122183c87149f30a1c290daf
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-01 22:30:02 -0300

    creates the UserService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,51 @@
-import { Model } from 'mongoose';
+import { PassportLocalModel } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './interfaces/user.interface';
+import { debug } from 'console';
+import { IUsersService } from './interfaces/user-service.interface';
+import { IUser } from './interfaces/user.interface';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {
-  constructor(@InjectModel('User') private userModel: Model<User>) {}
+export class UsersService implements IUsersService {
+  constructor(@InjectModel('User') private readonly userModel: PassportLocalModel<IUser>) {}
 
-  async findOne(email: string): Promise<User | undefined> {
-    return await this.userModel.findOne({ email }).exec();
+  async findAll(): Promise<IUser[]> {
+    return await this.userModel.find().exec();
+  }
+
+  async findOne(options: object): Promise<IUser> {
+    return await this.userModel.findOne(options).exec();
+  }
+
+  async findById(ID: number): Promise<IUser> {
+    return await this.userModel.findById(ID).exec();
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<IUser> {
+    const createdUser = new this.userModel(createUserDto);
+    return await createdUser.save();
+  }
+
+  async update(ID: number, newValue: IUser): Promise<IUser> {
+    const user = await this.userModel.findById(ID).exec();
+
+    if (!user._id) {
+      debug('user not found');
+    }
+
+    await this.userModel.findByIdAndUpdate(ID, newValue).exec();
+    return await this.userModel.findById(ID).exec();
+  }
+
+  async delete(ID: number): Promise<string> {
+    try {
+      await this.userModel.findByIdAndRemove(ID).exec();
+      return 'The user has been deleted';
+    }
+    catch (err) {
+      debug(err);
+      return 'The user could not be deleted';
+    }
   }
 }
