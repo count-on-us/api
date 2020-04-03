aedfaf57709e8babbd0727259ec2de0d8052427c
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-02 21:40:24 -0300

    extens user entity to hashPassword, compare and return a user interface

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { IUser } from './interfaces/user.interface';
 
 @Entity()
 export class User {
@@ -19,4 +26,25 @@ export class User {
 
   @Column({ default: true })
   isActive: boolean;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  async comparePassword(attempt: string): Promise<boolean> {
+    return await bcrypt.compare(attempt, this.password);
+  }
+
+  toResponseObject(): IUser {
+    const { id, firstName, lastName, email } = this;
+    const responseObject: IUser = {
+      id,
+      firstName,
+      lastName,
+      email,
+    };
+
+    return responseObject;
+  }
 }
