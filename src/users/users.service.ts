c67e23dab794f90459dc89818c6d40e4195ecb6a
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-02 22:10:38 -0300

    implements register methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -24,5 +25,18 @@ export class UsersService {
 
   async remove(id: string): Promise<void> {
     await this.usersRepository.delete(id);
+  }
+
+  async register(userDto: CreateUserDto): Promise<User> {
+    const { email } = userDto;
+    let user = await this.usersRepository.findOne({ where: { email } });
+    if (user) {
+      throw new HttpException(
+        'User already exists',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    user = await this.usersRepository.create(userDto);
+    return await this.usersRepository.save(user);
   }
 }
