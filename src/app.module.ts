492d4ea11e7962f4c2bf20f4eda0fd37e5ae5cad
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-13 22:05:08 -0300

    register Participant as an entity on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { UsersModule } from './users/users.module';
 import configuration from './config/configuration';
 import { User } from './users/user.entity';
 import { ParticipantModule } from './participant/participant.module';
+import { Participant } from './participant/participant.entity';
 
 @Module({
   imports: [
@@ -21,7 +22,7 @@ import { ParticipantModule } from './participant/participant.module';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         ...configService.get('database'),
-        entities: [User],
+        entities: [User, Participant],
       }),
       inject: [ConfigService],
     }),
