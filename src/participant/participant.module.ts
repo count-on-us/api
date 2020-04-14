54c24a83c0978dfe7c72b3108e913ece6cf21552
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-13 22:05:42 -0300

    register the Participant entity in the participant module.

--- src/participant/participant.module.ts
+++ src/participant/participant.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ParticipantService } from './participant.service';
 import { ParticipantController } from './participant.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Participant } from './participant.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Participant])],
   providers: [ParticipantService],
   controllers: [ParticipantController]
 })
