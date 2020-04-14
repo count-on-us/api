08fc189d2f3653a1e2d561164ebdc41d461a1eb9
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-13 22:06:03 -0300

    implements the participant service

--- src/participant/participant.service.ts
+++ src/participant/participant.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
-
+import { Repository } from 'typeorm';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Participant } from './participant.entity';
+import { IParticipantService } from './interfaces/participant-service.interface';
+import { CreateParticipantDto } from './dtos/create-participant.dto';
+import { UpdateParticipantDto } from './dtos/update-user.dto';
 @Injectable()
-export class ParticipantService {}
+export class ParticipantService implements IParticipantService {
+  constructor(
+    @InjectRepository(Participant)
+    private participantRepository: Repository<Participant>,
+  ) {}
+
+  async findAll(): Promise<Participant[]> {
+    return await this.participantRepository.find();
+  }
+
+  async findByEmail(email: string): Promise<Participant> {
+    return await this.participantRepository.findOne({ email });
+  }
+
+  async findOne(id: number): Promise<Participant> {
+    return await this.participantRepository.findOne(id);
+  }
+
+  async create(participantDto: CreateParticipantDto): Promise<Participant> {
+    const { email } = participantDto;
+    let participant = await this.participantRepository
+      .findOne({ where: { email } });
+
+    if (participant) {
+      throw new HttpException(
+        'This email is already taken.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    participant = this.participantRepository.create(participantDto);
+    return await this.participantRepository.save(participant);
+  }
+
+  async update(
+    id: number,
+    participantDto: UpdateParticipantDto
+  ): Promise<Participant> {
+    const { email } = participantDto;
+
+    let participant = await this.participantRepository
+      .findOne({ where: { email } });
+
+    if (participant.id !== id) {
+      throw new HttpException(
+        'This email is already taken.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    participant = this.participantRepository.create(participantDto);
+    return await this.participantRepository.save(participant);
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.participantRepository.delete(id);
+  }
+}
