cead262e380d861e697f969b59f591ea2000ea29
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-14 22:32:03 -0300

    creates the route to register a participant.

--- src/participant/participant.controller.ts
+++ src/participant/participant.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Response,
+  Body,
+  HttpStatus
+} from '@nestjs/common';
+import { ParticipantService } from './participant.service';
+import { CreateParticipantDto } from './dtos/create-participant.dto';
 
 @Controller('participant')
-export class ParticipantController {}
+export class ParticipantController {
+  constructor(private readonly participantService: ParticipantService) {}
+
+  @Post()
+  public async register(@Response() res, @Body() participant: CreateParticipantDto) {
+    const result = await this.participantService.register(participant);
+
+    if (!result.success){
+      return res.status(HttpStatus.BAD_REQUEST).json(result);
+    }
+
+    return res.status(HttpStatus.OK).json(result);
+  }
+}
