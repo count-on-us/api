22ec9391a3752562c86dc5db92c53070c34ef073
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-16 20:48:04 -0300

    makes the participant fields optional

--- src/participants/dtos/update-participant.dto.ts
+++ src/participants/dtos/update-participant.dto.ts
@@ -1,9 +1,11 @@
-import { MaxLength, IsEmail } from "class-validator";
+import { MaxLength, IsEmail, IsOptional } from "class-validator";
 
 export class UpdateParticipantDto {
   @MaxLength(100)
+  @IsOptional()
   readonly name: string;
 
   @IsEmail()
+  @IsOptional()
   readonly email: string;
 }
