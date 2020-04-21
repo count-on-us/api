9e4823969c2a9fbc9ffb42bd96671999b82db0a9
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-20 21:17:33 -0300

    documents create user DTO

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsIn,
@@ -10,29 +11,54 @@ import {
 export class CreateUserDto {
   @IsNotEmpty()
   @MaxLength(100)
+  @ApiProperty({
+    description: 'The user name.',
+    maxLength: 100,
+    example: 'John Doe',
+  })
   readonly name: string;
 
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The user email.',
+    example: 'user@domain.com',
+  })
   readonly email: string;
 
   @IsIn(['Psicólogo', 'Psicanalista', 'Psiquiatra'])
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The user profession.',
+    example: 'Psicólogo',
+  })
   readonly profession: string;
 
   @Matches(/([\w]+|[\d]+)\/[A-Z][A-Z]/)
   @IsNotEmpty()
   @MaxLength(100)
+  @ApiProperty({
+    description: 'The user license number.',
+    example: '123456/SP',
+  })
   readonly licenseNumber: string;
 
   @IsNumberString()
   @IsNotEmpty()
   @MaxLength(30)
+  @ApiProperty({
+    description: 'The user phone.',
+    example: '11999999999',
+  })
   readonly phone: string;
 
   @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/, {
     message: 'Password must be between 8 and 20 characters; must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character, but cannot contain whitespace.',
   })
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The user password',
+    example: '123p@assWord',
+  })
   readonly password: string;
 }
