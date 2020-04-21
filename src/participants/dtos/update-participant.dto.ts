322655221cc8eb9cb4fb6f32594fc8ce63c203e1
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-21 14:49:51 -0300

    documents update participant DTO

--- src/participants/dtos/update-participant.dto.ts
+++ src/participants/dtos/update-participant.dto.ts
@@ -1,11 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { MaxLength, IsEmail, IsOptional } from "class-validator";
 
 export class UpdateParticipantDto {
   @MaxLength(100)
   @IsOptional()
+  @ApiProperty({
+    description: 'The participant name.',
+    maxLength: 100,
+    example: 'Maria Doe',
+  })
   readonly name: string;
 
   @IsEmail()
   @IsOptional()
+  @ApiProperty({
+    description: 'The participant email.',
+    example: 'participant@domain.com'
+  })
   readonly email: string;
 }
