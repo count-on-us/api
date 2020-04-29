572e1288bff2b142fbc4ff8550b3f67b3beed240
Esron Dtamar <esron.dtamar@gmail.com>
2020-04-28 21:28:29 -0300

    implements recaptcha service

--- src/recaptcha/recaptcha.service.ts
+++ src/recaptcha/recaptcha.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IRecaptchaApiResponse } from './interfaces/recaptcha-api-response.interface';
 
 @Injectable()
-export class RecaptchaService {}
+export class RecaptchaService {
+  constructor(
+    private httpService: HttpService,
+    private configService: ConfigService
+  ) {}
+
+  async validateCapcha(responseToken: string): Promise<IRecaptchaApiResponse>
+  {
+    const response = await this.httpService.post(
+      'https://www.google.com/recaptcha/api/siteverify',
+      {
+        secret: this.configService.get('googleApiKey'),
+        response: responseToken,
+      }
+    ).toPromise();
+
+    console.log(response);
+
+    return response.data;
+  }
+}
