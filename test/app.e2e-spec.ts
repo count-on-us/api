5d1a61eeb44403f8818da63f6004dcb264fcbad8
Esron Dtamar <esron.dtamar@gmail.com>
2020-06-09 17:33:46 -0300

    fix teste e2e 404 root route

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,7 +18,10 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(404);
+  });
+
+  afterAll(async () => {
+    await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error;
   });
 });
