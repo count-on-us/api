9c91c8600efabd2ec0ad7f4969366ce4e4ecd24a
Esron Dtamar <esron.dtamar@gmail.com>
2020-06-12 16:01:51 -0300

    removes user empty user controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { User } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   exports: [UsersService, TypeOrmModule],
-  controllers: [UsersController],
 })
 export class UsersModule {}
