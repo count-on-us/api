2e73e186d6f50c4b104f254ec958d9c02b8cac05
Esron Dtamar <esron.dtamar@gmail.com>
2020-06-12 12:27:39 -0300

    fix reorder imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
 @Module({
