fd1abd1d7310e7951c410bb590cba9d819cec357
Esron Dtamar <esron.dtamar@gmail.com>
2020-06-14 14:10:26 -0300

    create a user array to the findAll test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -4,6 +4,25 @@ import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 
+const usersArray = [
+  new User(
+    'test1@test.com',
+    '123456',
+    'Test User 1',
+    'Psicólogo',
+    '123456/BA',
+    '74999999999'
+  ),
+  new User(
+    'test2@test.com',
+    '123456',
+    'Test User 2',
+    'Psicólogo',
+    '123456/BA',
+    '74999999999'
+  )
+]
+
 describe('UsersService', () => {
   let service: UsersService;
 
