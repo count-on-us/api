0a4669e35a60065b5afe9b042f7375bdf070e08b
Esron Dtamar <esron.dtamar@gmail.com>
2020-06-14 14:10:12 -0300

    import user related classes

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
